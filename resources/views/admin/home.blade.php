6dc6b65ec9d5bd1560f59ddc6609a771b22aa1a9
Radoan Hossain Prem <radoanhossaiinpream@yahoo.com>
2017-02-28 02:20:47 +0600

    Admin Dashboard Updated

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,17 +1,309 @@
-@extends('layouts.app')
+@extends('admin.layout.master')
+
+@section('breadcrumbs')
+    <ul class="page-breadcrumb">
+        <li>
+            <a href="#">Home</a>
+            <i class="fa fa-circle"></i>
+        </li>
+        <li>
+            <a href="{{url('')}}/admin/dashboard">Dashboard</a>
+        </li>
+    </ul>
+@endsection
+
+@section('title')
+    <h1 class="page-title"> Admin Dashboard
+        <small>statistics</small>
+    </h1>
+@endsection
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Admin Dashboard</div>
 
-                <div class="panel-body">
-                    You are logged in as <strong>Admin</strong>
+    <!-- BEGIN DASHBOARD STATS 1-->
+    <div class="row">
+        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+            <a class="dashboard-stat dashboard-stat-v2 blue" href="#">
+                <div class="visual">
+                    <i class="fa fa-comments"></i>
                 </div>
-            </div>
+                <div class="details">
+                    <div class="number">
+                        <span data-counter="counterup" data-value="100">0</span>
+                    </div>
+                    <div class="desc"> Total ACM-Solver </div>
+                </div>
+            </a>
+        </div>
+        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+            <a class="dashboard-stat dashboard-stat-v2 red" href="#" style="background-color: #1a3548">
+                <div class="visual">
+                    <i class="fa fa-bar-chart-o"></i>
+                </div>
+                <div class="details">
+                    <div class="number">
+                        <span data-counter="counterup" data-value="500"></span> </div>
+                    <div class="desc"> Total problem to solve </div>
+                </div>
+            </a>
+        </div>
+        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+            <a class="dashboard-stat dashboard-stat-v2 blue" href="#" style="background-color: #38956A">
+                <div class="visual">
+                    <i class="fa fa-shopping-cart"></i>
+                </div>
+                <div class="details">
+                    <div class="number">
+                        <span data-counter="counterup" data-value="300">0</span>
+                    </div>
+                    <div class="desc"> Problem done </div>
+                </div>
+            </a>
+        </div>
+
+        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+            <a class="dashboard-stat dashboard-stat-v2 blue" href="#" style="background-color: #965131">
+                <div class="visual">
+                    <i class="fa fa-bar-chart-o"></i>
+                </div>
+                <div class="details">
+                    <div class="number">
+                        <span data-counter="counterup" data-value="200"></span> </div>
+                    <div class="desc"> Total developer </div>
+                </div>
+            </a>
+        </div>
+
+        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+            <a class="dashboard-stat dashboard-stat-v2 green" href="#">
+                <div class="visual">
+                    <i class="fa fa-shopping-cart"></i>
+                </div>
+                <div class="details">
+                    <div class="number">
+                        <span data-counter="counterup" data-value="300">0</span>
+                    </div>
+                    <div class="desc"> Project done </div>
+                </div>
+            </a>
+        </div>
+
+        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+            <a class="dashboard-stat dashboard-stat-v2 purple" href="#">
+                <div class="visual">
+                    <i class="fa fa-globe"></i>
+                </div>
+                <div class="details">
+                    <div class="number">
+                        <span data-counter="counterup" data-value="50"></span> </div>
+                    <div class="desc"> New Members past month </div>
+                </div>
+            </a>
         </div>
     </div>
-</div>
+    <div class="clearfix"></div>
+    <!-- END DASHBOARD STATS 1-->
+
+    <div class="row">
+
+        <div class="col-lg-12 col-xs-12 col-sm-12">
+            <!-- BEGIN PORTLET-->
+            <div class="portlet light bordered">
+                <div class="portlet-title">
+                    <div class="caption">
+                        <i class="icon-share font-red-sunglo hide"></i>
+                        <span class="caption-subject font-dark bold uppercase">Revenue</span>
+                        <span class="caption-helper">monthly stats...</span>
+                    </div>
+                </div>
+                <div class="portlet-body">
+                    <div id="site_activities_loading">
+                        <img src="../assets/global/img/loading.gif" alt="loading" /> </div>
+                    <div id="site_activities_content" class="display-none">
+                        <div id="site_activities" style="height: 228px;"> </div>
+                    </div>
+                    <div style="margin: 20px 0 10px 30px">
+                        <div class="row">
+                            <div class="col-md-3 col-sm-3 col-xs-6 text-stat">
+                                <span class="label label-sm label-success"> Total Revenue: </span>
+                                <h3>$222</h3>
+                            </div>
+                            <div class="col-md-3 col-sm-3 col-xs-6 text-stat">
+                                <span class="label label-sm label-info"> Transaction fee: </span>
+                                <h3>$222</h3>
+                            </div>
+
+                            <div class="col-md-3 col-sm-3 col-xs-6 text-stat">
+                                <span class="label label-sm label-warning"> Orders: </span>
+                                <h3>222</h3>
+                            </div>
+
+                            <div class="col-md-3 col-sm-3 col-xs-6 text-stat">
+                                <span class="label label-sm label-danger"> Discount: </span>
+                                <h3>222</h3>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- subscription -->
+            <div class="portlet light bordered">
+                <div class="portlet-title">
+                    <div class="caption">
+                        <i class="icon-share font-red-sunglo hide"></i>
+                        <span class="caption-subject font-dark bold uppercase">Subscription Revenue</span>
+                        <span class="caption-helper">monthly stats...</span>
+                    </div>
+                </div>
+                <div class="portlet-body">
+                    <div id="site_activities_loading_subscription">
+                        <img src="../assets/global/img/loading.gif" alt="loading" /> </div>
+                    <div id="site_activities_content_subscription" class="display-none">
+                        <div id="site_activities_subscription" style="height: 228px;"> </div>
+                    </div>
+                    <div style="margin: 20px 0 10px 30px">
+                        <div class="row">
+
+                            <div class="col-md-3 col-sm-3 col-xs-6 text-stat">
+                                <span class="label label-sm label-danger"> Subscription Revenue: </span>
+                                <h3>32</h3>
+                            </div>
+
+                            <div class="col-md-3 col-sm-3 col-xs-6 text-stat">
+
+                            </div>
+
+                            <div class="col-md-3 col-sm-3 col-xs-6 text-stat">
+                                <span class="label label-sm label-warning"> Orders: </span>
+                                <h3>323</h3>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+@endsection
+
+@section('footer_scripts')
+    <script src="/assets/global/plugins/amcharts/amcharts/amcharts.js" type="text/javascript"></script>
+    <script src="/assets/global/plugins/amcharts/amcharts/serial.js" type="text/javascript"></script>
+    <script src="/assets/global/plugins/amcharts/amcharts/pie.js" type="text/javascript"></script>
+
+    <script src="/assets/global/plugins/amcharts/amcharts/themes/light.js" type="text/javascript"></script>
+    <script src="/assets/global/plugins/amcharts/amcharts/themes/patterns.js" type="text/javascript"></script>
+    <script src="/assets/global/plugins/amcharts/amcharts/themes/chalk.js" type="text/javascript"></script>
+    <script src="/assets/global/plugins/amcharts/ammap/ammap.js" type="text/javascript"></script>
+    <script src="/js/scripts/charts-amcharts.js" type="text/javascript"></script>
+
+
+    <script src="/assets/global/plugins/flot/jquery.flot.min.js" type="text/javascript"></script>
+    <script src="/assets/global/plugins/flot/jquery.flot.resize.min.js" type="text/javascript"></script>
+    <script src="/assets/global/plugins/flot/jquery.flot.categories.min.js" type="text/javascript"></script>
+    <script src="/assets/global/plugins/flot/jquery.flot.pie.min.js" type="text/javascript"></script>
+    <script src="/assets/global/plugins/flot/jquery.flot.stack.min.js" type="text/javascript"></script>
+    <script src="/assets/global/plugins/flot/jquery.flot.crosshair.min.js" type="text/javascript"></script>
+    <script src="/assets/global/plugins/flot/jquery.flot.axislabels.js" type="text/javascript"></script>
+    <!-- END PAGE LEVEL PLUGINS -->
+    <script type="text/javascript">
+        var ChartsFlotcharts = function() {
+
+            return {
+                //main function to initiate the module
+
+                init: function() {
+
+                    App.addResizeHandler(function() {
+                        Charts.initPieCharts();
+                    });
+
+                },
+
+                initCharts: function() {
+
+                    if (!jQuery.plot) {
+                        return;
+                    }
+
+                    var data = [];
+                    var totalPoints = 250;
+
+                    // random data generator for plot charts
+
+                    function getRandomData() {
+                        if (data.length > 0) data = data.slice(1);
+                        // do a random walk
+                        while (data.length < totalPoints) {
+                            var prev = data.length > 0 ? data[data.length - 1] : 50;
+                            var y = prev + Math.random() * 10 - 5;
+                            if (y < 0) y = 0;
+                            if (y > 100) y = 100;
+                            data.push(y);
+                        }
+                        // zip the generated y values with the x values
+                        var res = [];
+                        for (var i = 0; i < data.length; ++i) {
+                            res.push([i, data[i]]);
+                        }
+
+                        return res;
+                    }
+
+
+                    //Interactive Chart
+
+                    function chart2() {
+
+
+                    }
+
+                    //graph
+
+                    chart2();
+
+                },
+
+
+            };
+        }();
+
+        jQuery(document).ready(function() {
+            ChartsFlotcharts.init();
+            ChartsFlotcharts.initCharts();
+        });
+    </script>
+
+
+    <script src="/assets/global/plugins/fullcalendar/fullcalendar.min.js" type="text/javascript"></script>
+    <script src="/assets/global/plugins/jquery-easypiechart/jquery.easypiechart.min.js" type="text/javascript"></script>
+    <script src="/assets/global/plugins/jquery.sparkline.min.js" type="text/javascript"></script>
+    <script src="/assets/global/plugins/jqvmap/jqvmap/jquery.vmap.js" type="text/javascript"></script>
+
+    <script src="/assets/global/plugins/jqvmap/jqvmap/maps/jquery.vmap.world.js" type="text/javascript"></script>
+
+    <script src="/assets/global/plugins/jqvmap/jqvmap/data/jquery.vmap.sampledata.js" type="text/javascript"></script>
+
+    <!-- BEGIN PAGE LEVEL SCRIPTS -->
+    <script src="/js/scripts/dashboard.js" type="text/javascript"></script>
+    <!-- END PAGE LEVEL SCRIPTS -->
+
+    <script src="/assets/global/plugins/bootstrap-datepicker/js/bootstrap-datepicker.min.js" type="text/javascript"></script>
+    <script src="/assets/global/plugins/datatables/datatables.min.js" type="text/javascript"></script>
+    <script src="/assets/global/plugins/datatables/plugins/bootstrap/datatables.bootstrap.js" type="text/javascript"></script>
+    <script src="/assets/pages/scripts/table-datatables-buttons.min.js" type="text/javascript"></script>
+
+
+
+    <script type="text/javascript">
+
+        $(".nav a").on("click", function(){
+            $(".nav").find(".active").removeClass("active");
+            $(this).parent().addClass("active");
+        });
+    </script>
+
+
+
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -20,8 +20,6 @@ Auth::routes();
 Route::get('/home', 'HomeController@index');
 
 Route::prefix('admin')->group(function (){
-
-
     Route::get('/login', 'Auth\AdminLoginController@showLoginForm')->name('admin.login');
     Route::post('/login', 'Auth\AdminLoginController@login')->name('admin.login.submit');
 
